fe4b0b435237dc01bab82f75b80cbca8cc492b2a
Alejo-develop <yeyito872@gmail.com>
2024-09-05 19:15:21 -0500

    Add register and label component

--- src/pages/public/registerPage/registerForm.component.tsx
+++ src/pages/public/registerPage/registerForm.component.tsx
@@ -1,5 +1,32 @@
-export default function RegisterComponent(){
-    return (
-        <h1>Register component</h1>
-    )
-}
+import LabelComponent from "./label.component";
+import "./stylesRegister/style.register.css";
+import { User, Lock, Mail } from "lucide-react";
+
+const RegisterComponent = () => {
+  return (
+    <div className="register">
+      <h1>Create account</h1>
+
+      <div className="container-form">
+        <form className="register-form">
+          <LabelComponent icon={<User style={{ position: 'relative', left: '35px', top: '7px'}} />} placeholder="Username" />
+          <LabelComponent icon={<Mail style={{ position: 'relative', left: '35px', top: '7px'}} />}  placeholder="Email"/>
+          <LabelComponent icon={<Lock style={{ position: 'relative', left: '35px', top: '7px'}} />} placeholder="Password"/>
+          <LabelComponent icon={<Lock style={{ position: 'relative', left: '35px', top: '7px'}} />} placeholder="Confirm password"/>
+
+          <button>
+            Submit
+          </button>
+        </form>
+
+        <h3>OR</h3>
+
+        <div className="google-container">
+          <h5>Certificacion con google</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterComponent;
