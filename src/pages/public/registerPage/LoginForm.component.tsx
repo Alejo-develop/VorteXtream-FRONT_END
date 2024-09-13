814b42db583394673bf15e8a9bcc063057289079
JulianVR09 <julianvillacis.02@gmail.com>
2024-09-13 15:06:07 -0500

    google login

--- src/pages/public/registerPage/LoginForm.component.tsx
+++ src/pages/public/registerPage/LoginForm.component.tsx
@@ -3,86 +3,52 @@ import { Lock } from "lucide-react";
 import "./styles/loginstyles.css";
 import InputLogin from "./components/Input.component";
 import { useState } from "react";
-import {
-  AuthResponse,
-  AuthResponseError,
-} from "../../../common/interfaces/authResponse.interface";
+import { AuthResponse, AuthResponseError } from "../../../common/interfaces/authResponse.interface";
 import { useAuth } from "../../../auth/auth.provider";
 import { UserPayload } from "../../../common/interfaces/user.interface";
-<<<<<<< HEAD
+import { useNavigate } from "react-router-dom";
 import { handleGoogleLogin } from "../../../common/components/google/googleLogin";
-import google from "../../../assets/svg/google.svg";
+import google from '../../../assets/svg/google.svg'
 
 const LoginComponent = () => {
-  const [username, setUsername] = useState<string>(" ");
-  const [password, setPassword] = useState<string>(" ");
-  const [errorResponse, setErrorResponse] = useState(" ");
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorResponse, setErrorResponse] = useState<string>('');
 
   const auth = useAuth();
+  const goTo = useNavigate();
 
-=======
-import { useNavigate } from "react-router-dom";
-
-const LoginComponent = () => {
-  const [ username, setUsername ] = useState<string>('')
-  const [ password, setPassword ] = useState<string>('')
-  const [ errorResponse, setErrorResponse ] = useState('')
-
-  const auth = useAuth()
-  const goTo = useNavigate()
-  
->>>>>>> 89a563fdc38e630837aa696aacf5eb41dda4e3ed
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-<<<<<<< HEAD
-      const response = await fetch("http://miurl.com", {
-        method: "POST",
-=======
-      const response = await fetch('http://localhost:3000/vortextream/auth/login', {
+      const response = await fetch("http://localhost:3000/vortextream/auth/login", {
         method: 'POST',
->>>>>>> 89a563fdc38e630837aa696aacf5eb41dda4e3ed
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           username,
-<<<<<<< HEAD
           password,
         }),
       });
 
-      if (!response.ok) {
-        const errorToJson = (await response.json()) as AuthResponseError;
-        setErrorResponse(errorToJson.body.error);
-        throw new Error(errorToJson.body.error);
-      }
-
-      const resToJson = (await response.json()) as AuthResponse;
-      const token = resToJson.body.token;
-=======
-          password
-        })
-      })    
-
       if(!response.ok){
         const errorToJson = await response.json() as AuthResponseError
-        const errorMessage = errorToJson?.body?.error || 'An unexpected error occurred';
+        const errorMessage = errorToJson?.error || 'An unexpected error occurred';
         setErrorResponse(errorMessage);
   
         throw new Error(errorMessage);
       }
+
 
       const resToJson = await response.json() as AuthResponse
       console.log(resToJson);
       const token = resToJson.token
       const user = resToJson.user as UserPayload
 
-      
-      auth.saveSessionInfo(user, token)
-      goTo('/')
->>>>>>> 89a563fdc38e630837aa696aacf5eb41dda4e3ed
+      auth.saveSessionInfo(user, token);
+      goTo('/');
     } catch (err) {
       console.log(err);
       setErrorResponse('An error occurred. Please try again.');
@@ -95,27 +61,19 @@ const LoginComponent = () => {
 
       {!!errorResponse && <div className="error-message">{errorResponse}</div>}
       <form className="login-form" onSubmit={handleSubmit}>
-        {/* input de username */}
-<<<<<<< HEAD
         <InputLogin
+          type="text"
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
           Icon={<User size={24} />}
         />
-=======
 
-        <InputLogin type="text" onChange={(e) => setUsername(e.target.value)} placeholder="Username" Icon={<User size={24} />} />
-
-        {/* input de password */}
         <InputLogin
+          type="password"
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           Icon={<Lock size={24} />}
         />
-
-
-        <InputLogin type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" Icon={<Lock size={24} />} />
-        {/* input de remember me y forgot password */}
 
         <div className="remember-forgot">
           <div className="remember-me">
@@ -128,20 +86,10 @@ const LoginComponent = () => {
           </a>
         </div>
 
-        <button
-          className="googleLogin"
-          type="button"
-          onClick={handleGoogleLogin}
-        >
-          <img
-            src={google}
-            alt="Google"
-            className="google-icon"
-          />
+        <button className="googleLogin" type="button" onClick={handleGoogleLogin}>
+          <img src={google} alt="Google" className="google-icon" />
           Login with Google
         </button>
-
-        {/* botón de login */}
 
         <div className="boton-container">
           <button className="login-button" type="submit">
